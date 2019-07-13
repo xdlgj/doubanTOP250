7ecd1fc9899e51fc1783c7768df36f96102dda74
xdl <13333759329@163.com>
2019-07-13 23:43:17 +0800

    新建入口文件

--- doubanSpider/items.py
+++ doubanSpider/items.py
@@ -11,4 +11,10 @@ import scrapy
 class DoubanspiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    # 序号， 电影名称， 电影介绍，星级，电影评论数，电影描述
+    serial_number = scrapy.Field()
+    moive_name = scrapy.Field()
+    introduce = scrapy.Field()
+    star = scrapy.Field()
+    evaluate = scrapy.Field()
+    describe = scrapy.Field()
